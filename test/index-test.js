81247220d979ec873961fd1f79e556858d868657
Taylor Williams <taylor@thewilliams.net>
2018-01-11 12:04:56 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -1,5 +1,5 @@
 var index = require("../index.js")
-
+/*
 describe('what-is-a-test', () => {
   describe('Name', () => {
     it('returns "Susan"', () => {
@@ -7,7 +7,6 @@ describe('what-is-a-test', () => {
     })
   })
 
-/*
   describe('Height', () => {
     it('is less than 40', () => {
       expect(index.height).toBeLessThan(40)
@@ -20,6 +19,5 @@ describe('what-is-a-test', () => {
       expect(index.message).toInclude(index.height)
     })
   })
-*/
 })
-
+*/
